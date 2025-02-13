764e34514cd6c22d9e83f2d8916f25c356e3ba86
Daniel <danielpr.dev@gmail.com>
2025-02-13 10:39:36 +0000

    Add cors middleware and basic config files

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.v1.api import api_router
+from app.api.router import api_router
 from fastapi.openapi.docs import get_swagger_ui_html
+from app.core.config import settings
 
-app = FastAPI(docs_url=None)
+
+app = FastAPI(docs_url=None, title=settings.TITLE)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/docs", include_in_schema=False)
@@ -15,4 +26,4 @@ async def custom_swagger_ui_html():
     )
 
 
-app.include_router(api_router)
+app.include_router(api_router, prefix="/api")
